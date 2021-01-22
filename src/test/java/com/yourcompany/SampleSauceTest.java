5bc610266dcd2364d1c90a472616c456a50ce76f
Kareem Missoumi <kareem@saucelabs.com>
2021-01-22 14:29:19 -0800

    Removed real devices to increase parallelism

--- src/test/java/com/yourcompany/SampleSauceTest.java
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -71,7 +71,7 @@ public class SampleSauceTest {
                 // new Object[]{"browser","safari", "latest", "macOS 10.13",""},
                 new Object[]{"browser","safari", "11.0", "macOS 10.12",""},
                 new Object[]{"browser","chrome", "76.0", "OS X 10.11",""},
-                new Object[]{"device","", "", "Android","Samsung.*"},
+                // new Object[]{"device","", "", "Android","Samsung.*"},
                 
         };
     }
